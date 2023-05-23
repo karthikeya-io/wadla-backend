ab663282cb348118c8248a539135aba96cdf4432
karthikeya-io <karthikeyabethu01@gmail.com>
2023-05-23 12:13:35 +0530

    admin schema

---
+++ models/admin.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+const crypto = require("crypto");
+const { v4: uuidv4 } = require("uuid");
+
+const adminSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    encry_password: {
+      type: String,
+      required: true,
+    },
+    salt: String,
+  },
+  { timestamps: true }
+);
+
+// virtual field
+adminSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidv4();
+    this.encry_password = this.securePassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+adminSchema.methods = {
+  authenticate: function (plainpassword) {
+    return this.securePassword(plainpassword) === this.encry_password;
+  },
+
+  securePassword: function (plainpassword) {
+    if (!plainpassword) return "";
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(plainpassword)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+};
+
+module.exports = mongoose.model("Admin", adminSchema);
