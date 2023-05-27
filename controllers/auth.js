61b7eefd55f1f57535f2f76af4b0da9ae2beff66
karthikeya-io <karthikeyabethu01@gmail.com>
2023-05-28 02:04:32 +0530

    routs and controller for basic auth fuction like signup and login

---
+++ controllers/auth.js
@@ -0,0 +1,101 @@
+const Admin = require("../models/admin");
+const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+
+exports.signup = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
+  const admin = new Admin(req.body);
+  try {
+    await admin.save();
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      err: "NOT able to save admin in DB",
+    });
+  }
+
+  return res.status(200).json({
+    message: "Admin created successfully",
+  });
+};
+
+exports.signin = async (req, res) => {
+    const { email, password } = req.body;
+    console.log(req.body);
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            error: errors.array()[0].msg,
+        });
+    }
+
+    try {
+        const admin = await Admin.findOne({ email });
+        if (!admin) {
+            return res.status(400).json({
+                error: "Admin email does not exists",
+            });
+        }
+        if (!admin.authenticate(password)) {
+            return res.status(401).json({
+                error: "Email and password do not match",
+            });
+        }
+        const token = jwt.sign({ _id: admin._id }, process.env.SECRET, {expiresIn: '1h'});
+        // put token in cookie
+        // http only cookie
+        // expiring time of cookie is 1 hour
+        res.cookie("token", token, { maxAge: 3600000 });
+
+        // send response to front end
+        const { _id, name } = admin;
+        return res.json({ token, admin: { _id, name, email } });
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({
+            error: "Email does not exists",
+        });
+    }
+}
+
+exports.signout = (req, res) => {
+    res.clearCookie("token");
+    res.json({
+        message: "Admin signout successfully",
+    });
+}
+
+// protected routes
+exports.isSignedIn = (req, res, next) => {
+    // console.log(req.headers);
+    // console.log(req.headers.authorizati
+    // on);
+    if (req.headers.authorization === undefined) {
+        return res.status(401).json({
+            error: "Access denied",
+        });
+    }
+    const token = req.headers.authorization.split(" ")[1];
+    // console.log(token);
+    if (!token) {
+        return res.status(401).json({
+            error: "Access denied",
+        });
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET);
+        req.admin = decoded;
+        next();
+    }
+    catch (err) {
+        return res.status(401).json({
+            error: "Access denied",
+        });
+    }
+}
---
+++ routes/auth.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const { signup, signin } = require('../controllers/auth');
+
+
+// use this route only when you need to create admin user manually 
+// later you can comment this route
+router.post(
+    '/signup', 
+    [
+        check('name', 'name should be at least 3 char').isLength({ min: 3 }),
+        check('email', 'email is required').isEmail(),
+        check('password', 'password should be at least 6 char').isLength({ min: 6 }),
+    ],
+    signup
+);
+
+router.post(
+    '/login',
+    [
+        check('email', 'email is required').isEmail(),
+        check('password', 'password field is required').isLength({ min: 1 }),
+    ],
+    signin
+);
+
+// router.get('/signout', signout);
+
+// router.get('/testroute', isSignedIn, (req, res) => {
+//     res.json(req.auth);
+// }
+// );
+
+module.exports = router;
