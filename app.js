74fa9bb02d125a53b257049ad5a6502845d8c8c0
karthikeya-io <karthikeyabethu01@gmail.com>
2023-05-29 01:10:00 +0530

    exposed authRoutes and eventRoutes and updated some 3rd party middleware

--- app.js
+++ app.js
@@ -3,6 +3,27 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+// import routes
+const authRoutes = require('./routes/auth');
+// const bannerRoutes = require('./routes/banner');
+const eventRoutes = require('./routes/event');
+
+// middleware
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: true,
+    credentials: true
+  }));
+
+// routes middleware
+app.use('/api', authRoutes);
+// app.use('/api', bannerRoutes);
+app.use('/api', eventRoutes);
 
 
 // connect to mongoDB
@@ -16,7 +37,6 @@ exports.dbConnect = mongoose.connect(process.env.DB_CONNECT, {
     console.log(err);
 });
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+app.get('/', (req, res) => res.send('Hello World!'));
